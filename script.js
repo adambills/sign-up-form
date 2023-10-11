afb0ec7cecd14a2f72d6da0d1483556b2770d066
a-bills <121698448+a-bills@users.noreply.github.com>
2023-10-10 21:09:25 -0400

    Add password validation

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const button = document.querySelector('button');
 const form = document.querySelector('form');
-
+const password = document.querySelector('#password');
+const passConfirm = document.querySelector('#confirm');
+const passwords = [password, passConfirm];
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
-    setTimeout(() => {
-        form.submit();
-    }, 400);
+    if (password.value !== passConfirm.value) {
+        password.focus();
+        password.setCustomValidity('Passwords did not match.');
+        password.reportValidity();
+        passwords.forEach(pass => {
+            pass.classList.add('error');
+            pass.value = null;
+            console.log(document.activeElement.getAttribute('type'));
+        });
+        return;
+    }
+    setTimeout(() => form.submit(), 400);
 })
+
+document.addEventListener('keyup', (e) => {
+    if(document.activeElement.getAttribute('type') === 'password') {
+        e.target.classList.remove('error');
+        password.setCustomValidity('');
+    }
+});
 
 button.addEventListener('click', () => {
     button.classList.add('clicked');
